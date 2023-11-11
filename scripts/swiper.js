16683dc0a41b028edcd9cc486f18bbb53651bc7b
aldm <d44.a1@yandex.ru>
2023-11-11 20:20:21 +0300

    1.7

---
+++ scripts/swiper.js
@@ -0,0 +1,31 @@
+new Swiper(".purchase__swiper", {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  loop: false,
+  allowTouchMove: true,
+  freeMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  //   breakpoints: {
+  //     // when window width is >= 320px
+  //     320: {
+  //       slidesPerView: 1,
+  //       spaceBetween: 0,
+  //       slideToClickedSlide: true,
+  //     },
+  //     // when window width is >= 480px
+  //     480: {
+  //       slidesPerView: 1,
+  //       spaceBetween: 0,
+  //       slideToClickedSlide: true,
+  //     },
+  //     // when window width is >= 640px
+  //     640: {
+  //       slidesPerView: 4,
+  //       spaceBetween: 0,
+  //       slideToClickedSlide: true,
+  //     },
+  //   },
+});
