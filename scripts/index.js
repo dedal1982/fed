8c038dca31925e8160dabaea1839ed437130e2ef
aldm <d44.a1@yandex.ru>
2023-11-17 21:00:31 +0300

    dwa

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+const tabItems = Array.from(document.querySelectorAll(".personal__left-item"));
+const formItems = Array.from(
+  document.querySelectorAll(".personal__form-right")
+);
+
+const clearActiveTabs = (element, className = "active") => {
+  element.find((item) => item.classList.remove(`${className}`));
+};
+
+const setActiveTabs = (element, index, className = "active") => {
+  element[index].classList.add(`${className}`);
+};
+
+const chekTab = (item, index) => {
+  item.addEventListener("click", () => {
+    clearActiveTabs(tabItems);
+    clearActiveTabs(formItems);
+
+    setActiveTabs(tabItems, index);
+    setActiveTabs(formItems, index);
+  });
+};
+tabItems.forEach(chekTab);
