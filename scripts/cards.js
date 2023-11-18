e54489b8d57648463829230cfae2f9d456ece32b
aldm <d44.a1@yandex.ru>
2023-11-18 15:18:47 +0300

    wda

---
+++ scripts/cards.js
@@ -0,0 +1,44 @@
+const initialCards = [
+  {
+    name: "GTA",
+    link: "https://fikiwiki.com/uploads/posts/2022-02/1644982266_2-fikiwiki-com-p-kartinki-gta-5-na-rabochii-stol-3.jpg",
+    price: "150",
+    count: "4 продукта",
+    icon: "https://avatars.mds.yandex.net/i?id=c07a002c53a5d2e1f5ce2a89d89377aeca52d511-9269009-images-thumbs&n=13",
+  },
+  {
+    name: "PUBG",
+    link: "https://i.pinimg.com/originals/f6/89/6e/f6896ee3f1f037c65f357afae009792d.jpg",
+    price: "200",
+    count: "5 продукта",
+    icon: "https://avatars.mds.yandex.net/i?id=c07a002c53a5d2e1f5ce2a89d89377aeca52d511-9269009-images-thumbs&n=13",
+  },
+  {
+    name: "ХУЙПОЙМИЧТО",
+    link: "https://klike.net/uploads/posts/2022-09/1662460319_j-1.jpg",
+    price: "300",
+    count: "6 продукта",
+    icon: "https://prtimes.jp/data/corp/27396/tmp-d22a7ece406f2b13fe0a7a0bd1489934-04067b95c597f78cd4a38ecad65a60a6.jpg",
+  },
+  {
+    name: "ТАКАЯ ЖЕ ХУЙНЯ",
+    link: "https://w.forfun.com/fetch/c8/c83a548fa9f5139531fd97fed9f5be68.jpeg",
+    price: "150",
+    count: "4 продукта",
+    icon: "https://avatars.mds.yandex.net/i?id=c07a002c53a5d2e1f5ce2a89d89377aeca52d511-9269009-images-thumbs&n=13",
+  },
+  {
+    name: "ВАЩЕ ХЗ",
+    link: "https://w-dog.ru/wallpapers/7/5/452741411668320/killzone--shadow-fall-killzone-v-plenu-sumraka-xelgast-helghast-soldat-gorod-megapolis-doma-nebo-kapyushon-maska-shlang-guerrilla-games-sony-computer-entertainment.jpg",
+    price: "250",
+    count: "1 продукта",
+    icon: "https://avatars.mds.yandex.net/i?id=c07a002c53a5d2e1f5ce2a89d89377aeca52d511-9269009-images-thumbs&n=13",
+  },
+  {
+    name: "ШЛЯПА КАКАЯ ТО",
+    link: "https://www.sunhome.ru/i/wallpapers/61/novaya-igra-crysis.orig.jpg",
+    price: "550",
+    count: "0 продукта",
+    icon: "https://avatars.mds.yandex.net/i?id=c07a002c53a5d2e1f5ce2a89d89377aeca52d511-9269009-images-thumbs&n=13",
+  },
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,56 @@
-const tabItems = Array.from(document.querySelectorAll(".personal__left-item"));
-const formItems = Array.from(
-  document.querySelectorAll(".personal__form-right")
-);
+// const tabItems = Array.from(document.querySelectorAll(".personal__left-item"));
+// const formItems = Array.from(
+//   document.querySelectorAll(".personal__form-right")
+// );
 
-const clearActiveTabs = (element, className = "active") => {
-  element.find((item) => item.classList.remove(`${className}`));
-};
+// const clearActiveTabs = (element, className = "active") => {
+//   element.find((item) => item.classList.remove(`${className}`));
+// };
 
-const setActiveTabs = (element, index, className = "active") => {
-  element[index].classList.add(`${className}`);
-};
+// const setActiveTabs = (element, index, className = "active") => {
+//   element[index].classList.add(`${className}`);
+// };
 
-const chekTab = (item, index) => {
-  item.addEventListener("click", () => {
-    clearActiveTabs(tabItems);
-    clearActiveTabs(formItems);
+// const chekTab = (item, index) => {
+//   item.addEventListener("click", () => {
+//     clearActiveTabs(tabItems);
+//     clearActiveTabs(formItems);
 
-    setActiveTabs(tabItems, index);
-    setActiveTabs(formItems, index);
-  });
-};
-tabItems.forEach(chekTab);
-/***********/
-const slider = document.getElementById("slider");
-const sliderValue = document.getElementById("sliderValue");
+//     setActiveTabs(tabItems, index);
+//     setActiveTabs(formItems, index);
+//   });
+// };
+// tabItems.forEach(chekTab);
+// /***********/
+// const slider = document.getElementById("slider");
+// const sliderValue = document.getElementById("sliderValue");
 
-slider.addEventListener("input", function () {
-  sliderValue.innerHTML = this.value;
+// slider.addEventListener("input", function () {
+//   sliderValue.innerHTML = this.value;
+// });
+// /*****************************/
+const itemTemplate = document.querySelector(".template").content;
+const elements = document.querySelector(".elements");
+
+function setEventListeners(htmlElement) {}
+/***создать карточку***/
+function createCard(item) {
+  const htmlElement = itemTemplate.cloneNode(true);
+  const imageCard = htmlElement.querySelector(".card-image");
+  const cardPrice = htmlElement.querySelector(".card-price");
+  const gameName = htmlElement.querySelector(".game-name");
+  const gameCount = htmlElement.querySelector(".game-count");
+  const gameIcon = htmlElement.querySelector(".card-label");
+  imageCard.src = item.link;
+  cardPrice.textContent = item.price;
+  gameName.textContent = item.name;
+  gameCount.textContent = item.count;
+  gameIcon.src = item.icon;
+  setEventListeners(htmlElement);
+  return htmlElement;
+}
+
+initialCards.forEach(function (item) {
+  const elementCreate = createCard(item);
+  elements.prepend(elementCreate);
 });
