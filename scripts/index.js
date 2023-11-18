1c57f81af9a1fc8aecc13cddd192c81f3378684c
aldm <d44.a1@yandex.ru>
2023-11-18 22:57:50 +0300

    dwa

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,37 @@
+const activateTab = (tabItems, formItems, className = "active") => {
+  const clearActiveTabs = (element) => {
+    element.forEach((item) => item.classList.remove(className));
+  };
+
+  const setActiveTabs = (element, index) => {
+    element[index].classList.add(className);
+  };
+
+  const chekTab = (item, index) => {
+    item.addEventListener("click", () => {
+      clearActiveTabs(tabItems);
+      clearActiveTabs(formItems);
+      setActiveTabs(tabItems, index);
+      setActiveTabs(formItems, index);
+    });
+  };
+
+  tabItems.forEach(chekTab);
+};
+
 const tabItems = Array.from(document.querySelectorAll(".personal__left-item"));
 const formItems = Array.from(
   document.querySelectorAll(".personal__form-right")
 );
 
-const clearActiveTabs = (element, className = "active") => {
-  element.find((item) => item.classList.remove(`${className}`));
-};
+activateTab(tabItems, formItems);
 
-const setActiveTabs = (element, index, className = "active") => {
-  element[index].classList.add(`${className}`);
-};
-
-const chekTab = (item, index) => {
-  item.addEventListener("click", () => {
-    clearActiveTabs(tabItems);
-    clearActiveTabs(formItems);
-
-    setActiveTabs(tabItems, index);
-    setActiveTabs(formItems, index);
-  });
-};
-tabItems.forEach(chekTab);
-/***********/
-const slider = document.getElementById("slider");
-const sliderValue = document.getElementById("sliderValue");
-
-slider.addEventListener("input", function () {
-  sliderValue.innerHTML = this.value;
-});
 // /*****************************/
+
+// const registrForm = document.getElementById("formReg");
+// const registrFormAuth = document.getElementById("formAuth");
+
+// document.getElementById("registr").onclick = function () {
+//   registrForm.classList.add("active");
+//   registrFormAuth.classList.remove("active");
+// };
